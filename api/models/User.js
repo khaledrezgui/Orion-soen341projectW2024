2868d7dd58368cbf8082175f0f5ed8a5f0a2506e
safaa <safaak.250@gmail.com>
2024-03-07 23:57:25 -0500

    User CRUD

---
+++ api/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    fullname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+
+});
+
+module.exports = mongoose.model("Users", UserSchema);
---
+++ api/routes/users.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const User = require("../models/User.js");
+const router = express.Router();
+
+//CREATE
+router.post("/", async (req, res) => {
+
+    const newUser = new User(req.body)
+
+    try {
+        const savedUser = await newUser.save();
+        res.status(200).json(savedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//UPDATE
+router.put("/:id", async (req, res) => {
+
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true });
+        res.status(200).json(updatedUser);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//DELETE
+router.delete("/:id", async (req, res) => {
+
+    try {
+        const updatedUser = await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted.");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//GET
+router.get("/:id", async (req, res) => {
+
+    try {
+        const user = await User.findById(req.params.id);
+        res.status(200).json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//GET ALL
+router.get("/", async (req, res) => {
+
+    try {
+        const users = await User.find();
+        res.status(200).json(users);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
