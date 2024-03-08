cb4f08ce23c0d589e1180ed6624c33c989759876
khaledrezgui <k_rezgui@live.concordia.ca>
2024-03-08 15:25:32 -0500

    Fixing issue in front end (Basic skeleton)

--- frontend/car_rental_app/src/App.js
+++ frontend/car_rental_app/src/App.js
@@ -49,7 +49,21 @@ function App() {
 
   <div className='container'>
 
-    <CarCard car1={cars[0]} />
+  <div className='car'>
+      <div>
+        <p>{car1.year}</p>
+      </div>
+      <div>
+        <img src={car1.poster !== "NA" ? car1.poster : 'https://via.placeholder.com/400'} alt = {car1.make}/>
+      </div>
+      <div>
+        <span>
+          {car1.price}
+        </span>
+        <h3>{car1.make}</h3>
+      </div>
+      
+    </div>
 
   </div>
 
