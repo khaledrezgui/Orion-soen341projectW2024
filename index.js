e4d02a2edcea7fe6b219b7a3d09e32e76c454f52
safaa <safaak.250@gmail.com>
2024-03-07 19:50:20 -0500

    Node.js MongoDB Connection

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to mongoDB.")
+    } catch (error) {
+        throw error
+    }
+};
+
+mongoose.connection.on("disconnected",()=>{
+    console.log("mongoDB disconnected!")
+})
+
+mongoose.connection.on("connected",()=>{
+    console.log("mongoDB connected!")
+})
+
+
+app.listen(8800, () => {
+    connect()
+    console.log("Connected to backend....");
+});
