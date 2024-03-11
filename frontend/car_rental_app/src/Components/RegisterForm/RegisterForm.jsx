b6508deb5ed60d69e63995c25d64b55f5cc467e9
khaledrezgui <k_rezgui@live.concordia.ca>
2024-03-11 18:24:57 -0400

    link between frontend and backend for reg/login

--- frontend/car_rental_app/src/Components/RegisterForm/RegisterForm.jsx
+++ frontend/car_rental_app/src/Components/RegisterForm/RegisterForm.jsx
@@ -1,45 +1,71 @@
-import React from "react";
-import './RegisterForm.css'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import './RegisterForm.css';
 import { FaUser } from "react-icons/fa";
-import { CiLock } from "react-icons/ci";
 import { CiMail } from "react-icons/ci";
-
-
+import { CiLock } from "react-icons/ci";
 
 const RegisterForm = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
-return(
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post('/auth/register', { username, email, password })
+      .then(response => {
+        console.log(response.data);
+        navigate('/login'); // Navigate to login after successful registration
+      })
+      .catch(err => {
+        console.error('Error during registration:', err.response);
+      });
+  };
 
-
-<div>
-<div className="wrapper">
-    <form action ="">
+  return (
+    <div className="wrapper">
+      <form onSubmit={handleSubmit}>
         <h1>Register</h1>
         <div className="input-box">
-            <input type="text" placeholder="Name" required />
-            <FaUser className="icon"/>
+          <input
+            type="text"
+            placeholder="Username"
+            required
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <FaUser className="icon"/>
         </div>
 
         <div className="input-box">
-            <input type="text" placeholder="email" required />
-            <CiMail className="icon"/>
-
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <CiMail className="icon"/>
         </div>
+        
         <div className="input-box">
-            <input type="text" placeholder="Password" required />
-            <CiLock className="icon"/>
-
+          <input
+            type="password"
+            placeholder="Password"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <CiLock className="icon"/>
         </div>
 
         <button type="submit">Register</button>
-
-
-    </form>
-</div>
-
-</div>
-);
-
+      </form>
+    </div>
+  );
 };
 
-export default RegisterForm
+export default RegisterForm;
