0b6bbde0ec6bd1fe9bc699d2ddc6f5a5cb4b464e
khaledrezgui <k_rezgui@live.concordia.ca>
2024-03-12 13:49:12 -0400

    Keeping track of who is logged in

--- frontend/car_rental_app/src/Components/CarBrowsing/CarCard.jsx
+++ frontend/car_rental_app/src/Components/CarBrowsing/CarCard.jsx
@@ -13,16 +13,17 @@ const CarCard = ({ car: { _id, make, model, year, type, price, description, phot
         <p>{year}</p>
       </div>
       <div>
-        <img src={photos.length > 0 ? photos[0] : "https://via.placeholder.com/400"} alt={`${make} ${model}`} />
+        <img src={photos.length > 0 ? photos[0] : "https://via.placeholder.com/400"} alt={`${make} ${model}`} style={{width: '100%', height: 'auto', objectFit: 'cover'}} />
       </div>
       <div>
         <span>{type}</span>
         <h3>{`${make} ${model}`}</h3>
-        <p>Availability: From {formatDate(availability.start)} to {formatDate(availability.end)}</p>
+        {availability && availability.length > 0 && (
+          <p>Availability: From {formatDate(availability[0].start)} to {formatDate(availability[0].end)}</p>
+        )}
         <p>Rental Price: ${parseFloat(price).toFixed(2)}/hr</p>
         <p>{description}</p>
         <p>Seats: {seats}</p>
-
         {isAvailable ? (
           <button>Book Reservation</button>
         ) : (
