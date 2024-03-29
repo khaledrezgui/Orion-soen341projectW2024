b6bfcd09fed8908fb0621aa5220c0d08897f4466
khaledrezgui <k_rezgui@live.concordia.ca>
2024-03-29 13:49:40 -0400

    Confirmation styling changes

--- frontend/car_rental_app/src/Components/ReservationLIst/AdminReservationLIst/CheckOut/CheckOutConfirmation.js
+++ frontend/car_rental_app/src/Components/ReservationLIst/AdminReservationLIst/CheckOut/CheckOutConfirmation.js
@@ -7,19 +7,19 @@ const CheckOutConfirmation = () => {
   const { message, reservationDetails } = location.state || {};
 
   return (
-    <div className="confirmation-container">
-      <h3 className="confirmation-header">Return Confirmation</h3>
-      {message && <p className="confirmation-message">{message}</p>}
+    <div className="checkout-confirmation-container">
+      <h3 className="checkout-confirmation-header">Return Confirmation</h3>
+      {message && <p className="checkout-confirmation-message">{message}</p>}
       {reservationDetails && (
-        <div className="reservation-details">
+        <div className="checkout-reservation-details">
           <p>Total Price: <span>${reservationDetails.totalPrice.toFixed(2)}</span> </p>
           <p>Car Model: <span>{reservationDetails.carModel}</span></p>
           <p>Start Date: <span>{new Date(reservationDetails.startDate).toDateString()} {new Date(reservationDetails.startDate).toTimeString()}</span> </p>
           <p>End Date: <span>{new Date(reservationDetails.endDate).toDateString()} {new Date(reservationDetails.endDate).toTimeString()}</span></p>
-          <p>GPS: <span className={(reservationDetails.gps === true) ? 'included' : 'not-included'}>{(reservationDetails.gps === true) ? 'Included' : 'Not Included'}</span></p>
-          <p>Child Safety Seat: <span className={(reservationDetails.safetySeat === true) ? 'included' : 'not-included'}>{(reservationDetails.safetySeat === true) ? 'Included' : 'Not Included'}</span></p>
-          <p>Fuel Service: <span className={(reservationDetails.fuelService === true) ? 'included' : 'not-included'}>{(reservationDetails.fuelService === true) ? 'Included' : 'Not Included'}</span></p>
-          <p>Insurance: <span className={(reservationDetails.insurance === true) ? 'included' : 'not-included'}>{(reservationDetails.insurance === true) ? 'Included' : 'Not Included'}</span></p>
+          <p>GPS: <span className={(reservationDetails.gps === true) ? 'checkout-included' : 'checkout-not-included'}>{(reservationDetails.gps === true) ? 'Included' : 'Not Included'}</span></p>
+          <p>Child Safety Seat: <span className={(reservationDetails.safetySeat === true) ? 'checkout-included' : 'checkout-not-included'}>{(reservationDetails.safetySeat === true) ? 'Included' : 'Not Included'}</span></p>
+          <p>Fuel Service: <span className={(reservationDetails.fuelService === true) ? 'checkout-included' : 'checkout-not-included'}>{(reservationDetails.fuelService === true) ? 'Included' : 'Not Included'}</span></p>
+          <p>Insurance: <span className={(reservationDetails.insurance === true) ? 'checkout-included' : 'checkout-not-included'}>{(reservationDetails.insurance === true) ? 'Included' : 'Not Included'}</span></p>
         </div>
       )}
     </div>
